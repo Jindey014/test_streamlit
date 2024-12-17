0b9c8d34ca1deb0d7ca49a835464c5dcd8b80e15
Jinish <96121491+Jindey014@users.noreply.github.com>
2024-12-17 11:10:28 +0545

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -21,10 +21,17 @@ with st.expander('Data'):
 
 with st.expander('Data Visualization'):
 
-  
   st.line_chart(data = df,x= 'bill_length_mm', y='body_mass_g' , color='species')
 
-  
+
+# Data Preparations
+with st.sidebar:
+  st.header('Input Parameters')
+# "island","bill_length_mm","bill_depth_mm","flipper_length_mm","body_mass_g"
+
+  island = st.selectbox('Island',('Biscoe','Dream','Torgerson'))
+  gender = st.selectbox('Gender',('Male','Female'))
+  bill_depth_mm = st.slider('Bill Length (mm)',32.1, 59.6 , 43.9)
 
  
 
