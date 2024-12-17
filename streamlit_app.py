a833f65fdc61f06122cf05658163a6610a62a73d
Jinish <96121491+Jindey014@users.noreply.github.com>
2024-12-17 13:09:12 +0545

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -84,6 +84,8 @@ clf.fit(X,y)
 ## Apply model to make predictions
 prediction = clf.predict(input_row)
 prediction_prob = clf.predict_proba(input_row)
+
+prediction_prob
 df_prediction_prob = pd.DataFrame(prediction_prob)
 
 
