b5395767caab414eb1fe5c4a09429e86f192104d
Jinish <96121491+Jindey014@users.noreply.github.com>
2024-12-17 11:32:12 +0545

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -35,7 +35,17 @@ with st.sidebar:
   bill_depth_mm = st.slider('Bill Depth (mm)', 13.1, 21.5, 17.2)
   flipper_length_mm = st.slider('Flipper length (mm)', 172.0,  231.0, 201.0)
   body_mass_g = st.slider('Body mass (g)', 2700.0, 6300.0 , 4207.0)
-  
+
+  #Creating a dataframe for the input parameters
+  data = {'island':island,
+         'bill_length_mm':bill_length_mm,
+         'bill_depth_mm':bill_width_mm,
+         'flipper_length_mm': flipper_length_mm,
+         'body_mass_g':body_mass_g,
+         'gender':gender}
+  input_df = pd.DataFrame(data, index[0])
+input_df
+
   
 
  
