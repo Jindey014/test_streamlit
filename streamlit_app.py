5cc067f3eab0823b8dd8f59cb275b016dcfbc139
Jinish <96121491+Jindey014@users.noreply.github.com>
2024-12-17 10:47:06 +0545

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,16 @@
 import streamlit as st
+import pandas as pd
+import numpy as np
 
-st.title('This is a Machine Learning App')
+st.title('Machine Learning App')
 
 st.info('This app diffrentiates penguins based on various parameters')
+
+df = pd.read_csv("https://raw.githubusercontent.com/dataprofessor/data/refs/heads/master/penguins_cleaned.csv)
+
+df
+
+
+
+
 
