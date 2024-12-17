148e845f2313e6930319b62570327a4a141483cc
Jinish <96121491+Jindey014@users.noreply.github.com>
2024-12-17 13:25:27 +0545

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -83,14 +83,15 @@ clf.fit(X,y)
 
 ## Apply model to make predictions
 prediction = clf.predict(input_row)
+prediction
 prediction_prob = clf.predict_proba(input_row)
 
 df_prediction_prob = pd.DataFrame(prediction_prob)
 # df_prediction_prob
 df_prediction_prob.columns = ['Adelie','Gentoo','Chinstrap']
-df_prediction_prob.rename(columns={0:'Adelie',
-                                  1:'Chinstrap',
-                                  2:'Gentoo'})
+# df_prediction_prob.rename(columns={0:'Adelie',
+#                                   1:'Chinstrap',
+#                                   2:'Gentoo'})
 
 df_prediction_prob
 
